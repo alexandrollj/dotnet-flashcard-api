83ad37c2649e875d73a67cf58c041673420a2d38
= <alexandrollj@gmail.com>
2025-05-06 08:13:17 -0600

    feat: Adding routes to program.cs

--- Program.cs
+++ Program.cs
@@ -1,22 +1,35 @@
 using FlashcardApi.Services;
+using FlashcardApi.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// We add controllers
-builder.Services.AddControllers();
 
 // Register in-memory repository
 builder.Services.AddSingleton<IFlashcardRepository, FlashcardRepository>();
 
 var app = builder.Build();
 
-// Enable routing to controllers
-app.MapControllers();
 
 // We add a simpple health-check endpoint
 app.MapGet("/", () => Results.Ok("Flashcard API is running!"));
+
+app.MapGet("/decks", (IFlashcardRepository repo) =>
+{
+    return repo.GetAllDecks();
+});
+
+app.MapGet("/decks/{id}", (Guid id, IFlashcardRepository repo) =>
+{
+    var deck = repo.GetDeck(id);
+    return deck is not null ? Results.Ok(deck) : Results.NotFound();
+});
+
+app.MapPost("/decks", (Deck deck, IFlashcardRepository repo) =>
+{
+    var newDeck = repo.CreateDeck(deck);
+    return Results.Created($"/decks/{newDeck.Id}", newDeck);
+});
 
 app.Run();
